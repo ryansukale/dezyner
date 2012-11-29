7a158272493fc4ac40bed1c21a35a744df3ae504
Ryan Sukale <ryansukale@gmail.com>
2012-11-29 15:52:25 -0500

    Modified the css for the popup dialog to use a net css3 checkered backgroud. Reused the code from lea verau's css 3 demo at http://lea.verou.me/css3patterns/#blueprint-grid however, the css is specific to chrome.

--- js/initStyles.js
+++ js/initStyles.js
@@ -1,1 +1,1 @@
-/*	javascript file that will be used to initialize the styles for the entire page*/
+/*	javascript file that will be used to initialize the styles for the entire page	*/$(function(){	//Clear the backgrounds in order to customize the background using css3	$('.ui-widget-content').addClass('grey-checks');	console.log('asd');});
